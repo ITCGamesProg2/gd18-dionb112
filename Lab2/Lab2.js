c12d3a28edfaaeec5a506c8092b3dd4b67f9c993
Don Buck <dion-b@hotmail.com>
2018-09-25 10:01:07 +0100

    Lab2-3 Array done

--- Lab2/Lab2.js
+++ Lab2/Lab2.js
@@ -6,11 +6,21 @@ function square(n)
 function howManyLightsabersDoYouOwn(name)
 {
 	result = 0;
-	if (name==='Zach' || name ==='zach')
+	if (name ==='Zach' || name ==='zach')
 	{
 		result = 18;
 	}
 	return result;
+}
+
+function getAverage(marks)
+{
+	var result = 0, i = 0;
+	for (i in marks)
+	{
+		result = result + marks[i];
+	}
+	return Math.floor(result / marks.length);
 }
 
 function main()
@@ -31,6 +41,24 @@ function main()
 	else
 	{
 		console.log("FAIL 2");
+	}
+
+	if (getAverage([2,2,2,2]) === 2)
+	{
+		console.log("OK 3.1");
+	}
+	else
+	{
+		console.log("FAIL 3.1");
+	}
+
+	if (getAverage([1,1,1,1,1,1,1,2]) === 1)
+	{
+		console.log("OK 3.2");
+	}
+	else
+	{
+		console.log("FAIL 3.2");
 	}
 
 }
