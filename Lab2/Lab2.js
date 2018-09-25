9d43327de5351cc06f4f40a48509b79d1c6a1ecd
Don Buck <dion-b@hotmail.com>
2018-09-25 09:36:09 +0100

    Lab 2-2 Lightsabers done

--- Lab2/Lab2.js
+++ Lab2/Lab2.js
@@ -3,14 +3,34 @@ function square(n)
   return n*n;
 }
 
+function howManyLightsabersDoYouOwn(name)
+{
+	result = 0;
+	if (name==='Zach' || name ==='zach')
+	{
+		result = 18;
+	}
+	return result;
+}
+
 function main()
 {
 	if(square(5)==25)
 	{
-		console.log("OK");
+		console.log("OK 1");
 	}	
 	else
 	{
-		console.log("square FAIL");
+		console.log("FAIL 1");
 	}
+
+	if (howManyLightsabersDoYouOwn("Zach") || howManyLightsabersDoYouOwn('zach') === 18)
+	{
+		console.log("OK 2");
+	}
+	else
+	{
+		console.log("FAIL 2");
+	}
+
 }
