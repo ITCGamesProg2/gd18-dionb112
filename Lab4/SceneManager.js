9cb978da1fa0e5387829bb1b01018d472dcc11f8
Don Buck <dion-b@hotmail.com>
2018-10-09 16:15:58 +0100

    Lab4-3 working (a lot of learning at once, break into parts)

--- Lab4/SceneManager.js
+++ Lab4/SceneManager.js
@@ -1,4 +1,31 @@
 class SceneManager
 {
-    
+    constructor(currentScene = null)
+    {
+        this.currentScene = currentScene;
+        this.scenes = {};
+        this.sceneTitles = [];
+        this.index = -1;
+    }
+    addScene(scene)
+    {
+        scenes['Scene ' + index] = scene;
+        sceneTitles[index] = scene;
+    }
+    goToScene(title)
+    {
+        //do more
+        this.currentScene = title;
+    }
+    goToNextScene()
+    {
+        if (index < this.sceneTitles.length)
+        {
+            index++
+        }
+        else
+        {
+            index = 0;
+        }
+    }
 }
