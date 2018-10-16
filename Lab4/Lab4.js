dbaf5cce051228079315c6e3c7669176ec60b548
Don Buck <dion-b@hotmail.com>
2018-10-16 11:00:01 +0100

    Lab 4 Complete #time 4h20m (Demo tidy, game not global when using bind()

--- Lab4/Lab4.js
+++ Lab4/Lab4.js
@@ -6,13 +6,13 @@
  */
 function main()
 {
-    this.game = new Game();
-    document.addEventListener("click", clickHandler);
+    var game = new Game();
+    document.addEventListener("click", clickHandler.bind(null, game));
 }
 /** function called to handle clicks,
  *  goes to next scene and renders it
  */
-function clickHandler(e)
+function clickHandler(game, e)
 {
     game.sceneManager.goToNextScene();
     game.sceneManager.render(game.ctx);
