5bab4b9b2465b79291bca152728ce37a665d642e
Don Buck <dion-b@hotmail.com>
2018-10-16 10:04:40 +0100

    Lab 4 Complete #time 4h20m

---
+++ Lab4/MenuScene.js
@@ -0,0 +1,12 @@
+class MenuScene extends Scene
+{
+    constructor(title, colour)
+    {
+        super(title, colour);
+    }
+    render(ctx)
+    {
+        ctx.font = '33px sans-serif';
+        super.render(ctx)
+    }
+}
