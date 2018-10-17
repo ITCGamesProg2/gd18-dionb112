b75e107fb0b45cca742b281d298786c3ea423d94
Don Buck <dion-b@hotmail.com>
2018-10-17 11:34:10 +0100

    Lab5-2 almost complete (fixed issue with maxing out stack)

--- Lab5/Game.js
+++ Lab5/Game.js
@@ -1,5 +1,7 @@
 // Global var (no const in js)
 CANVAS_SIZE = 1000;
+MOVEMENT = 42;
+
 /**
  * Game class which controls instance of SceneManager,
  * adding in all scene objects and rendering first scene
@@ -10,10 +12,8 @@ class Game
     constructor() 
     {        
         this.ctx = {};
-        this.initWorld();
         this.boundRecursiveUpdate = this.update.bind(this);
         this.boundDraw = this.draw.bind(this);
-
     }
     /**
      * See canvas as local var here but ctx as object of this class
@@ -21,6 +21,8 @@ class Game
      */
     initWorld()
     {
+        console.log('Initialising Game World');
+        this.player = new Player(42,42,42,42, 255);
         var canvas = document.createElement("canvas");
         canvas.id = 'mycanvas';
         canvas.width = CANVAS_SIZE;
@@ -30,19 +32,47 @@ class Game
         // append to HTML document
         document.body.appendChild(canvas);
         this.ctx.font = '42px arial';
-        console.log('Initialising Game World');
-
+        // this prevents scrolling (default behaviour of some keys)
+        window.addEventListener("keydown", function(e)
+        {
+            // Space and arrow keys
+            if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) 
+            {
+                e.preventDefault();
+            }
+        }, false); 
+        document.addEventListener("keydown", this.keyDownHandler.bind(null, this.player));
+ 
     }
     update()
     {
         this.boundDraw();
         console.log('game updating...');
         // recursion, currently maxing out call stack size *
-        window.requestAnimationFrame(this.boundRecursiveUpdate());
+        window.requestAnimationFrame(this.boundRecursiveUpdate);
     }
     draw()
     {
         console.log('game drawing...');
+        this.player.draw(this.ctx);
+    }
+    keyDownHandler(e, player)
+    {
+        switch (e.keyCode)
+        {
+        case 38:
+            player.move(0,-MOVEMENT);
+            break;
+        case 37:
+            player.move(-MOVEMENT, 0);
+            break;
+        case 39:
+            player.move(MOVEMENT, 0);
+            break;
+        case 40:
+            player.move(0, MOVEMENT);
+            break;
+        }
     }
 
 }
--- Lab5/Lab5.js
+++ Lab5/Lab5.js
@@ -8,15 +8,6 @@
 function main()
 {
     var game = new Game();
+    game.initWorld();
     game.update();
-    document.addEventListener("keydown", clickHandler.bind(null, game));
-}
-
-/** function called to handle clicks,
- *  goes to next scene and renders it
- */
-function clickHandler(game, e)
-{
-    game.sceneManager.goToNextScene();
-    game.sceneManager.render(game.ctx);
 }
