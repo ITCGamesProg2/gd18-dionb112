8571773f73c76198025be3312779410cddae783c
Don Buck <dion-b@hotmail.com>
2018-10-30 11:57:15 +0000

    Lab 7 complete # time 2h 30m ~~

--- Lab7/main.js
+++ Lab7/main.js
@@ -5,7 +5,10 @@ const CANVAS_SIZE = 1000;
  * use device sim
  * then more advanced touch events
  */
-// Patryk, it's good but you could have made this with greatly simplified code
+// Patryk - it's good but you could have made this with greatly simplified code
+// Dion - I should take this one board, even as I was creating this based on the linked 
+// mozilla developer touch event page I thought it seemed overly complex and 
+// chose to simplify certain elements from that page in my own implementation
 function main()
 {
     this.ongoingTouches = [];
