c89923b986e564f7aa61a786adb3257e9c81a220
Don Buck <dion-b@hotmail.com>
2018-11-13 15:35:06 +0000

    Lab 8-2 done ( could not get load listener to work though )

--- Lab8/Game.js
+++ Lab8/Game.js
@@ -4,12 +4,19 @@
 class Game
 {
     constructor() 
-    {        
+    {    
+        //this.isLoaded = false;
+        this.spriteStrip =  new Image();
         this.ctx = {};
         this.boundRecursiveUpdate = this.update.bind(this);
+        this.boundDraw = this.draw.bind(this);
     }
     initWorld()
     {
+        /**this.spriteStrip.addEventListener('load', function() {
+            this.isLoaded = true;
+        }, false);*/
+        this.spriteStrip.src = 'spritestrip.png';
         var canvas = document.createElement("canvas");
         canvas.id = 'mycanvas';
         canvas.width = CANVAS_SIZE;
@@ -22,15 +29,18 @@ class Game
     }
     update()
     {
-        console.log('game updating...');
+        //console.log('game updating...');
         // recursion, currently maxing out call stack size *
         window.requestAnimationFrame(this.boundRecursiveUpdate);
-        this.draw();
+        this.boundDraw();
     }
     draw()
     {
-        console.log('game drawing...');
-        this.ctx.clearRect(0,0,CANVAS_SIZE, CANVAS_SIZE);  
-        this.levelComplete();
+        //console.log('game drawing...');
+        this.ctx.clearRect(0,0,CANVAS_SIZE, CANVAS_SIZE); 
+        //if (this.isLoaded)
+        {
+            this.ctx.drawImage(this.spriteStrip, 42, 42);
+        }
     }
 }
--- Lab8/main.js
+++ Lab8/main.js
@@ -1,7 +1,10 @@
-const CANVAS_SIZE = 1000;
+const CANVAS_SIZE = 1600;
 /**
  * @author Dion Buckley
  * Lab to Draw and Animate a sprite
+ * Here using one image;
+ * for multiple images simply implement answer 2 (~24 points) here:
+ * https://stackoverflow.com/questions/3032299/checking-for-multiple-images-loaded
  */
 function main()
 {
